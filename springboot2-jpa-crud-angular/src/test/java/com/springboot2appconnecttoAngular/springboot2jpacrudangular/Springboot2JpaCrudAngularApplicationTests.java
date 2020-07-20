12c9842061475628e2801aee87ef66cd78b0d1b3
Doleith Chambers <dchambers@fslads.fsl.org.jm>
2020-07-20 10:54:05 -0500

    Angular and Springboot connected to MySQL

---
+++ springboot2-jpa-crud-angular/src/test/java/com/springboot2appconnecttoAngular/springboot2jpacrudangular/Springboot2JpaCrudAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot2appconnecttoAngular.springboot2jpacrudangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
